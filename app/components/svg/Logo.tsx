45d2599e367a8cc43132b5715124b8d966e826a3
ingdanielj0816 <danielj0816@outlook.com>
2023-08-04 22:56:12 -0500

    .

--- app/components/svg/Logo.tsx
+++ app/components/svg/Logo.tsx
@@ -10,7 +10,7 @@ const Logo: React.FC<LogoProps> = ({ width = 150, height = 150 }) => {
     return (
         <Image
             className={`w-${width} h-${height}`}
-            src="/logor1.png"
+            src="/projects/logor1.png"
             alt="Logo"
             width={width}
             height={height}
