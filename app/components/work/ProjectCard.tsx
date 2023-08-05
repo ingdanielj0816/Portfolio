61834d9684da32dda447b04b1fd0a5d529ed929b
ingdanielj0816 <danielj0816@outlook.com>
2023-08-04 22:18:16 -0500

    .

--- app/components/work/ProjectCard.tsx
+++ app/components/work/ProjectCard.tsx
@@ -82,7 +82,7 @@ const ProjectCard = ({
                     className={`absolute text-white  ${!(id % 2 === 0)
                         ? "right-0 top-32 mr-0 ml-10 md:right-0 md:ml-0 lg:right-0 lg:top-60  lg:mr-4"
                         : "left-10 top-32 ml-0 md:mr-12 lg:top-52 lg:ml-4"
-                            } mb-10  md:mb-16 lg:mb-14 `}>
+                    } mb-10  md:mb-16 lg:mb-14 `}>
                     <AnimatedTitle
                         text={name}
                         className={
